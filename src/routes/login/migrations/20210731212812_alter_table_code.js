fc5104b910c5fc0ff13b3bdf0466b5b0b973a43d
Dimitry Ivanov <2@ivanoff.org.ua>
2022-07-31 17:41:37 +0300

    update migrations

--- src/routes/login/migrations/20210731212812_alter_table_code.js
+++
@@ -1,9 +0,0 @@
-exports.up = (knex) => knex.schema
-  .alterTable('code', (table) => {
-    table.string('code', 128).alter();
-  });
-
-exports.down = () => (knex) => knex.schema
-  .alterTable('code', (table) => {
-    table.string('code', 6).alter();
-  });
